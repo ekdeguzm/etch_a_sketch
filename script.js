1a7db2cc9d5f08d51774f755b9a4677f3788bf5b
Kawayan <kawayan.deguzman@gmail.com>
2022-03-01 15:23:03 -0800

    Add comments to make 16x16 grid on script.js file

---
+++ script.js
@@ -0,0 +1,5 @@
+// Create a webpage with 16x16 grid of square divs
+// Create a function that lays down 16 divs next to eachother
+// then moves onto next line down,
+// repeats process until 16 rows are made.
+
