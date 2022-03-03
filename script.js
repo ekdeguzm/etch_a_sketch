5def8aa8ff766bc9c5e401c829947d843a0f6af0
Kawayan <kawayan.deguzman@gmail.com>
2022-03-03 10:23:39 -0800

    Create js, css, and html files. Add function to create divs

--- script.js
+++ script.js
@@ -2,4 +2,19 @@
 // Create a function that lays down 16 divs next to eachother
 // then moves onto next line down,
 // repeats process until 16 rows are made.
+let vw = 0
+
+// Create a single square 
+function createDivSquare() {
+    for (let i = 0; i < 3; i++) {
+        const container = document.querySelector('#container');
+        let div = document.createElement("div");
+        div.classList.add('square');
+
+        div.style.left += vw;
+        container.appendChild(div);
+    }
+}
+
+createDivSquare()
 
